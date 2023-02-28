55d0df96ec20324e72c351c7e7e154b27ed9ace5
davidfullard <davidfullard417@gmail.com>
2023-02-28 09:21:20 -0500

    spiff initial commit

---
+++ src/progress_bar_exercise/ProgressBarExercise.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Exercise from "../exercise/Exercise";
+
+const ProgressBarExercise = () => {
+  return (
+    <div className="progress-bar-exercise">
+      <Exercise
+        solution={<Solution />}
+        specsUrl="https://github.com/SpiffInc/spiff_react_exercises/issues/1"
+        title="Progress Bar Exercise"
+      />
+    </div>
+  );
+};
+
+export default ProgressBarExercise;
+
+// ----------------------------------------------------------------------------------
+
+const Solution = () => {
+  return <div>Add solution here</div>;
+};
